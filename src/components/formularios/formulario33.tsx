af426014a833a6f34a552509b0b25f24ddc058bd
Marlon Masabanda <marlon25.masabanda@gmail.com>
2025-03-14 02:55:03 -0500

    fix: final state

--- src/components/formularios/formulario33.tsx
+++ src/components/formularios/formulario33.tsx
@@ -181,8 +181,15 @@ export default function UploadForm() {
         }
       }
     );
-
     toast.success("Proceso guardado exitosamente");
+    // guardado final
+    saveFinalState({
+      ...json,
+      jsonData: JSON.stringify({
+        autores: formDataAutores,
+        productos: formDataProductos,
+      }),
+    });
     await bonita.changeTask();
   };
 
